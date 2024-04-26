92303e9e52e50f41aee170ff7c650dc7fa72b727
Anurag Singh <iitpatelanurag2003@gmail.com>
2024-04-27 02:52:22 +0530

    important

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux"
-import FetchItem from "../component/FetchItems"
-import Footer from "../component/Footer"
-import Header from "../component/Header"
+import FetchItem from "./component/FetchItems"
+import Footer from "./component/Footer"
+import Header from "./component/Header"
 import { Outlet } from "react-router-dom"
-import Loading from "./Loading"
+import Loading from "./routes/Loading"
 function App() {
   const fetchState=useSelector(store=>store.fetchStatus.currentlyFetching);
   return (
